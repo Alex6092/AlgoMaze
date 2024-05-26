59e9b74851ad1bae472e08837cf4e961e088770b
Alexandre GREMONT <alexandre.gremont@gmail.com>
2024-05-26 17:25:54 +0200

    Add gems, switches and 4 direction character logic and AI art

--- square/algomaze.js
+++ square/algomaze.js
@@ -1,3 +1,70 @@
+const Direction = {
+    Up: 'Up',
+    Down: 'Down',
+    Left: 'Left',
+    Right: 'Right'
+};
+
+const SwitchState = {
+    On: "On",
+    Off: "Off"
+}
+
+function Gem(x, y)
+{
+    this.x = x;
+    this.y = y;
+    this.collected = false;
+    this.image = Loader.getImage('gem');
+}
+
+Gem.prototype.getImage = function() {
+    return this.image;
+}
+
+Gem.prototype.isCollected = function() {
+    return this.collected;
+}
+
+Gem.prototype.collect = function() {
+    this.collected = true;
+}
+
+Gem.prototype.reset = function() {
+    this.collected = false;
+}
+
+function Switch(x, y, initialState)
+{
+    this.x = x;
+    this.y = y;
+    this.initialState = initialState;
+    this.state = this.initialState;
+
+    this.images = {}
+    this.images[SwitchState.On] = Loader.getImage('switch_on');
+    this.images[SwitchState.Off] = Loader.getImage('switch_off');
+}
+
+Switch.prototype.getImage = function() {
+    return this.images[this.state];
+}
+
+Switch.prototype.reset = function() {
+    this.state = this.initialState;
+}
+
+Switch.prototype.toggle = function() {
+    if(this.state == SwitchState.On)
+    {
+        this.state = SwitchState.Off;
+    }
+    else
+    {
+        this.state = SwitchState.On;
+    }
+}
+
 var map = {
     cols: 12,
     rows: 12,
@@ -29,6 +96,9 @@ var map = {
         4, 4, 4, 0, 5, 4, 4, 4, 4, 4, 4, 4,
         4, 4, 4, 0, 0, 3, 3, 3, 3, 3, 3, 3
     ]],
+
+    switches: [ { x: 128, y: 192, state: false }, { x: 256, y: 192, state: true} ],
+    gems: [ { x: 128, y: 64 }],
     getTile: function (layer, col, row) {
         return this.layers[layer][row * map.cols + col];
     },
@@ -61,16 +131,27 @@ var map = {
 function Hero(map, x, y) {
     this.startX = x;
     this.startY = y;
+    this.startDirection = Direction.Down;
     this.map = map;
     this.x = x;
     this.y = y;
     this.width = map.tsize;
     this.height = map.tsize;
 
-    this.image = Loader.getImage('hero');
+    this.images = {};
+    this.images[Direction.Down] = Loader.getImage('hero_down');
+    this.images[Direction.Up] = Loader.getImage('hero_up');
+    this.images[Direction.Left] = Loader.getImage('hero_left');
+    this.images[Direction.Right] = Loader.getImage('hero_right');
+
+    this.direction = this.startDirection;
 }
 
-Hero.SPEED = 256; // pixels per second
+Hero.SPEED = 192; // pixels per second
+
+Hero.prototype.getImage = function() {
+    return this.images[this.direction];
+}
 
 Hero.prototype.move = function (delta, dirx, diry) {
     // move hero
@@ -89,6 +170,7 @@ Hero.prototype.move = function (delta, dirx, diry) {
 
 Hero.prototype.goDown = function(delta, onFinished)
 {
+    this.direction = Direction.Down;
     var curX = Math.floor((this.x - map.tsize / 2) / map.tsize);
     var curY = Math.floor((this.y - map.tsize / 2) / map.tsize);
 
@@ -106,6 +188,7 @@ Hero.prototype.goDown = function(delta, onFinished)
 
 Hero.prototype.goUp = function(delta, onFinished)
 {
+    this.direction = Direction.Up;
     var curX = Math.ceil((this.x - map.tsize / 2) / map.tsize);
     var curY = Math.ceil((this.y - map.tsize / 2) / map.tsize);
 
@@ -123,6 +206,7 @@ Hero.prototype.goUp = function(delta, onFinished)
 
 Hero.prototype.goLeft = function(delta, onFinished)
 {
+    this.direction = Direction.Left;
     var curX = Math.ceil((this.x - map.tsize / 2) / map.tsize);
     var curY = Math.ceil((this.y - map.tsize / 2) / map.tsize);
 
@@ -140,6 +224,7 @@ Hero.prototype.goLeft = function(delta, onFinished)
 
 Hero.prototype.goRight = function(delta, onFinished)
 {
+    this.direction = Direction.Right;
     var curX = Math.floor((this.x - map.tsize / 2) / map.tsize);
     var curY = Math.floor((this.y - map.tsize / 2) / map.tsize);
 
@@ -159,6 +244,27 @@ Hero.prototype.resetPosition = function()
 {
     this.x = this.startX;
     this.y = this.startY;
+    this.direction = this.startDirection;
+}
+
+Hero.prototype.turnLeft = function()
+{
+    if(this.direction == Direction.Down)
+    {
+        this.direction = Direction.Right;
+    }
+    else if(this.direction == Direction.Right)
+    {
+        this.direction = Direction.Up;
+    }
+    else if(this.direction == Direction.Up)
+    {
+        this.direction = Direction.Left;
+    }
+    else if(this.direction == Direction.Left)
+    {
+        this.direction = Direction.Down;
+    }
 }
 
 Hero.prototype._collide = function (dirx, diry) {
@@ -199,7 +305,16 @@ Hero.prototype._collide = function (dirx, diry) {
 Game.load = function () {
     return [
         Loader.loadImage('tiles', '../assets/tiles.png'),
-        Loader.loadImage('hero', '../assets/character.png')
+
+        Loader.loadImage('hero_down', '../assets/down.png'),
+        Loader.loadImage('hero_up', '../assets/up.png'),
+        Loader.loadImage('hero_left', '../assets/left.png'),
+        Loader.loadImage('hero_right', '../assets/right.png'),
+
+        Loader.loadImage('gem', '../assets/gem.png'),
+
+        Loader.loadImage('switch_off', '../assets/switch_off.png'),
+        Loader.loadImage('switch_on', '../assets/switch_on.png')
     ];
 };
 
@@ -210,6 +325,19 @@ Game.init = function () {
 
     this.actionQueue = new Array();
     this.hero = new Hero(map, 160, 160);
+    this.switches = [];
+    for(var i = 0; i < map.switches.length; i++)
+    {
+        _switch = map.switches[i];
+        this.switches.push(new Switch(_switch.x, _switch.y, _switch.state ? SwitchState.On : SwitchState.Off));
+    }
+
+    this.gems = [];
+    for(var i = 0; i < map.gems.length; i++)
+    {
+        gem = map.gems[i];
+        this.gems.push(new Gem(gem.x, gem.y));
+    }
 };
 
 Game.update = function (delta) {
@@ -217,29 +345,71 @@ Game.update = function (delta) {
     {
         var actionName = this.actionQueue[0];
 
-        if(actionName == "BAS")
+        if(actionName == "DOWN")
         {
             this.hero.goDown(delta, function() {
                 Game.actionQueue.shift();
             });
         }
-        else if(actionName == "HAUT")
+        else if(actionName == "UP")
         {
             this.hero.goUp(delta, function() {
                 Game.actionQueue.shift();
             });
         }
-        else if(actionName == "GAUCHE")
+        else if(actionName == "LEFT")
         {
             this.hero.goLeft(delta, function() {
                 Game.actionQueue.shift();
             });
         }
-        else if(actionName == "DROITE")
+        else if(actionName == "RIGHT")
         {
             this.hero.goRight(delta, function() {
                 Game.actionQueue.shift();
             });
+        }
+        else if(actionName == "AVANCER")
+        {
+            var direction = this.hero.direction;
+            this.actionQueue[0] = direction.toUpperCase();
+        }
+        else if(actionName == "TURN_LEFT")
+        {
+            this.hero.turnLeft();
+            Game.actionQueue.shift();
+        }
+        else if(actionName == "TOGGLE_SWITCH")
+        {
+            var x = this.hero.x - map.tsize / 2;
+            var y = this.hero.y - map.tsize / 2;
+
+            for(var i = 0; i < this.switches.length; i++)
+            {
+                _switch = this.switches[i];
+                if(_switch.x == x && _switch.y == y)
+                {
+                    _switch.toggle();
+                }
+            }
+
+            Game.actionQueue.shift();
+        }
+        else if(actionName == "COLLECT_GEM")
+        {
+            var x = this.hero.x - map.tsize / 2;
+            var y = this.hero.y - map.tsize / 2;
+
+            for(var i = 0; i < this.gems.length; i++)
+            {
+                gem = this.gems[i];
+                if(gem.x == x && gem.y == y)
+                {
+                    gem.collect();
+                }
+            }
+
+            Game.actionQueue.shift();
         }
     }
 };
@@ -289,16 +459,38 @@ Game._drawGrid = function () {
     }
 };
 
+Game._drawGemsAndSwitches = function()
+{
+    for(var i = 0; i < this.switches.length; i++)
+    {
+        var _switch = this.switches[i];
+        this.ctx.drawImage(_switch.getImage(), _switch.x, _switch.y);
+    }
+
+    for(var i = 0; i < this.gems.length; i++)
+    {
+        var gem = this.gems[i];
+        if(!gem.isCollected())
+        {
+            this.ctx.drawImage(gem.getImage(), gem.x, gem.y);
+        }
+    }
+}
+
 Game.render = function () {
     // draw map background layer
     this._drawLayer(0);
 
+    // draw gems and switches 
+    this._drawGemsAndSwitches();
+
     // draw main character
     this.ctx.drawImage(
-        this.hero.image,
+        this.hero.getImage(),
         this.hero.x - this.hero.width / 2,
         this.hero.y - this.hero.height / 2);
 
+    
     // draw map top layer
     this._drawLayer(1);
 
@@ -309,30 +501,64 @@ Game.queueAction = function(actionName) {
     this.actionQueue.push(actionName);
 };
 
+Game.reset = function() {
+    this.actionQueue = [];
+    this.hero.resetPosition();
+    for(var i = 0; i < this.switches.length; i++)
+    {
+        this.switches[i].reset();
+    }
 
+    for(var i = 0; i < this.gems.length; i++)
+    {
+        this.gems[i].reset();
+    }
+}
+
+/*
 function bas()
 {
-    Game.queueAction("BAS");
+    Game.queueAction("DOWN");
 }
 
 function haut()
 {
-    Game.queueAction("HAUT");
+    Game.queueAction("UP");
 }
 
 function gauche()
 {
-    Game.queueAction("GAUCHE");
+    Game.queueAction("LEFT");
 }
 
 function droite()
 {
-    Game.queueAction("DROITE");
+    Game.queueAction("RIGHT");
+}
+*/
+
+function moveForward()
+{
+    Game.queueAction("AVANCER");
+}
+
+function turnLeft()
+{
+    Game.queueAction("TURN_LEFT");
+}
+
+function toggleSwitch()
+{
+    Game.queueAction("TOGGLE_SWITCH");
+}
+
+function collectGem()
+{
+    Game.queueAction("COLLECT_GEM");
 }
 
 function solve(code)
 {
-    Game.hero.resetPosition();
+    Game.reset();
     eval(code);
-    
 }
